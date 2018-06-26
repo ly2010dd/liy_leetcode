58e29f53b580d80ade08ddbd5787eefb3612f2c6
liy <liyang2009dd@163.com>
2018-06-26 22:50:22 +0800

    S3

---
+++ java/_S3_LengthOfLongestSubstring/Solution.java
@@ -0,0 +1,28 @@
+import java.lang.System;
+import java.lang.String;
+import java.lang.StringBuffer;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("String: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(String.valueOf(lengthOfLongestSubstring(input)));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int retLength = 0;
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            while ( sb.indexOf(String.valueOf(s.charAt(i))) != -1 ) {
+                retLength = sb.length() > retLength ? sb.length() : retLength;
+                sb.delete(0, 1);
+            }
+            sb.append(s.charAt(i));
+       }
+       retLength = sb.length() > retLength ? sb.length() : retLength;
+       return retLength;
+    }
+}
